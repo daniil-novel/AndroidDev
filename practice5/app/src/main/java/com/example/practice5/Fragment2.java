483f19c939068155b3959f751a7f09e43a8067c3
Daniil <daniilpriwesentsew@gmail.com>
2023-05-01 22:51:31 +0300

    Practice5 full implemented

---
+++ practice5/app/src/main/java/com/example/practice5/Fragment2.java
@@ -0,0 +1,47 @@
+package com.example.practice5;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+
+public class Fragment2 extends Fragment {
+
+    private NavController navController;
+    private SharedViewModel viewModel;
+    private TextView textView;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_2, container, false);
+
+        Button button = view.findViewById(R.id.button_2);
+        textView = view.findViewById(R.id.title_textview);
+
+        textView.setText(getArguments().getString("amount"));
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                navController.navigate(R.id.action_fragment2_to_fragment3);
+            }
+        });
+
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        navController = Navigation.findNavController(view);
+    }
+}
